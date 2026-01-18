b459be5fb65461b9f243884f893b6ac5ffa572e6
Berlin Brown <berlin.brown@gmail.com>
2026-01-18 09:04:35 -0500

    Adding basic cellular automata example
    Adding basic cellular automata example

---
+++ java2d/cellular/src/main/java/com/berlin/cellular/square/AutomataMain.java
@@ -0,0 +1,11 @@
+package com.berlin.cellular.square;
+
+public class AutomataMain {
+
+    public static void main(final String [] args) {
+        System.out.println(">>> Running application");
+        final Cellular automata = new Cellular();
+        automata.invokeLater();
+    }
+
+}
